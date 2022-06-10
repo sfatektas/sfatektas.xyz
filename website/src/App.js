77ff834f1b8181e93ef4346e4e08e9b649212408
Sefa <sfatektas55@gmail.com>
2022-06-10 19:06:47 +0300

    preloader added.

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,30 @@
+import ReactLoading from 'react-loading';
+import React from "react";
+import { useState,useEffect } from "react";
 import Aside from "./components/Aside";
 import Content from "./components/Content";
+
 function App() {
+
+const [done , setDone] = useState(false);
+
+useEffect(()=>
+{
+  setTimeout(()=>{
+    setDone(true);
+  },2000)
+},[])
+
   return (
-    <>
-      <div className="sm:flex sm:flex-col">
-      <Aside/>
-      <Content/>
-      </div>
+    !done ?
+    <div className=' h-screen flex justify-center items-center bg-black'>
+      <ReactLoading type='cylon' color='#374151' delay={200}/>
+    </div>
+    : <>
+    <div className="sm:flex sm:flex-col">
+    <Aside/>
+    <Content/>
+    </div>
     </>
   );
 }
